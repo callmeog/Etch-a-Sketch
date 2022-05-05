c2c3494d1b832e3254c760deb31b251e8f1f5454
callmeog01 <ogb.official1@gmail.com>
2022-05-05 14:56:06 -0400

    Created grid col and appended row to container div

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ function grid(elem) {
    const container = document.querySelector('#container');
 
    //creating grid rows
-   for(i = 0; i < 16; i+=1) {  
+   for(let i = 0; i < 16; i+=1) {  
       let row = document.createElement('div') ;
       row.className = "row";
       row.id = "row" + i;
-   }
 
+      //creating columns
+      for(let x = 0; x < 16; x++) {
+         let col = document.querySelector('div');
+         col.className = 'col';
+         row.appendChild(col);
+      }
+      container.appendChild(row);
+   }
+   return container;
 }
 
 
