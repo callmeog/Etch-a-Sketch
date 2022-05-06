d67cf8c71f9f7e53847ca57b61f627e3705b602a
callmeog01 <ogb.official1@gmail.com>
2022-05-06 01:08:20 -0400

    Fixed for loop

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ function grid(elem) {
    const container = document.querySelector('#container');
 
    //creating grid rows
-   for(let i = 0; i < 16; i+=1) {  
+   for(let i = 0; i < 16; i++) {  
       let row = document.createElement('div') ;
-      row.className = 'row';    
+      row.className = 'row';  
+      row.style.padding = '1em'
 
       //creating columns
-      for(let x = 0; x < 16; x+=1) {
+      for(let x = 0; x < 16; x++) {
          let col = document.createElement('div');
          col.className = 'col';
+         col.style.padding = '1em'
          row.appendChild(col);  
       }
       container.appendChild(row);
