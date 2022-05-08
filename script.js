6e93dda547fd08bf06c641b3bbce7a846b1c67a9
callmeog01 <ogb.official1@gmail.com>
2022-05-08 14:56:39 -0400

    Fixed grid function

--- script.js
+++ script.js
@@ -1,39 +1,29 @@
 "use strict";
-function grid(elem) {
-   const container = document.querySelector('#container');
-   const colorPallet = document.querySelector('.color-pallet');
-   const fullGridpallet = document.querySelector('.full--grid-pallet')
+function drawGrid(container, row, col) {
+   const box = document.querySelector('div');
+   box.className = 'box';
+   box.id = `box${row}${col}`;
 
-   //creating grid rows
-   for(let i = 0; i < 16; i++) {  
-      let row = document.createElement('div') ;
-      row.className = 'row';
-
-      //creating columns
-      for(let x = 0; x < 16; x++) {
-         let col = document.createElement('div');
-         col.className = 'col';
-         row.appendChild(col);
-
-         //change background color on hover
-         col.addEventListener('mouseenter', () => {
-            col.style.backgroundColor = colorPallet.value;
-         });
-      }
-      container.appendChild(row);
-   }
-   elem.appendChild(container)
-   container.style.cssText = 'width: 40%; margin: auto';
-
-
+   container.appendChild(box);
+   return(box);
 }
-grid(document.body);
+
+
+
+
+
+/*
+for(let i = 0; i < 16; i++) {  
+      for(let j = 0; j < 16; x++) {
+         
+      }
+      
+   }
+   */
+
 
 
 const gridButton = document.querySelector('#grid-range');
-
-
-
 gridButton.addEventListener('click', (element) => {
    element = prompt('enter grid size')
 });
