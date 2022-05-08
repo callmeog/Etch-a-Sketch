f84419883b453a11765b512b9425db50f447c792
callmeog01 <ogb.official1@gmail.com>
2022-05-08 10:02:32 -0400

    remove onclick and add mouceenter

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 function grid(elem) {
    const container = document.querySelector('#container');
+   const colorPallet = document.querySelector('.color-pallet');
+   const fullGridpallet = document.querySelector('.full--grid-pallet')
 
    //creating grid rows
    for(let i = 0; i < 16; i++) {  
@@ -11,18 +13,27 @@ function grid(elem) {
       for(let x = 0; x < 16; x++) {
          let col = document.createElement('div');
          col.className = 'col';
-         row.appendChild(col);  
+         row.appendChild(col);
+
+         //change background color on hover
+         col.addEventListener('mouseenter', () => {
+            col.style.backgroundColor = colorPallet.value;
+         });
       }
       container.appendChild(row);
    }
    elem.appendChild(container)
    container.style.cssText = 'width: 40%; margin: auto';
+
+
 }
 grid(document.body);
 
 
-const gridButton = document.querySelector('#container');
+const gridButton = document.querySelector('#grid-range');
+
+
 
 gridButton.addEventListener('click', (element) => {
-
+   element = prompt('enter grid size')
 });
