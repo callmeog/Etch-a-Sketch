4ebab215f9d4b683638ff2dbf759e6614ac1f957
callmeog01 <ogb.official1@gmail.com>
2022-05-08 15:06:56 -0400

    Fixed grid row and column

--- script.js
+++ script.js
@@ -12,21 +12,21 @@ function createBox() {
    const grid = document.createElement('div');
    grid.className = 'grid';
 
-   for(let i = 0; i < 16; i ++) {
-      
+   for(let i = 0; i < 16; i++) {
+      for(let j = 0; j < 16; j++);
+
+      drawGrid(grid, i, j);
    }
+   container.appendChild(grid);
 }
 
+function gridHolder() {
+   const game = document.querySelector('#game');
+   createBox(game)
 
+}
+gridHolder()
 
-/*
-for(let i = 0; i < 16; i++) {  
-      for(let j = 0; j < 16; x++) {
-         
-      }
-      
-   }
-   */
 
 
 
