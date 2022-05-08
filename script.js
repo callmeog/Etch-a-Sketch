b6f6fef692091a7c027adcb8bd1e0a72cc4eaf52
callmeog01 <ogb.official1@gmail.com>
2022-05-08 05:39:21 -0400

    Fixed container width to 40%

--- script.js
+++ script.js
@@ -4,19 +4,18 @@ function grid(elem) {
    //creating grid rows
    for(let i = 0; i < 16; i++) {  
       let row = document.createElement('div') ;
-      row.className = 'row';  
-      row.style.padding = '1em'
+      row.className = 'row';
 
       //creating columns
       for(let x = 0; x < 16; x++) {
          let col = document.createElement('div');
          col.className = 'col';
-         col.style.padding = '1em'
          row.appendChild(col);  
       }
       container.appendChild(row);
    }
    elem.appendChild(container)
+   container.style.cssText = 'width: 40%; margin: auto';
 }
 grid(document.body);
 
